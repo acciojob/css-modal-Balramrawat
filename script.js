54f99894fc034bd3cae2eb79813ee5b55859ad81
Balramrawat <110815042+Balramrawat@users.noreply.github.com>
2025-02-06 23:56:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let openbtn=document.getElementById("openModal")
+        let modal=document.getElementById("myModal")
+        let closebtn=document.querySelector(".close-modal")
+
+
+        openbtn.addEventListener("click",()=>{
+            modal.style.display="block";
+        });
+
+        closebtn.addEventListener("click",()=>{
+            modal.style.display="none"
+        });
+
+        window.addEventListener("click",(e)=>{
+            if(e.target === modal){
+                modal.style.display="none";
+            }
+        })
